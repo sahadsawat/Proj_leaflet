e139ed1fcfcb4ad9378bc73e239221b41c22329a
sahadsawat <mr.fapman_nongnine@hotmail.com>
2023-04-02 10:17:15 +0700

    add to server success

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,4 +1,6 @@
 class MyConstant {
-  String domain = '192.168.1.5:80';
+  // String domain = 'http://192.168.1.5';
+  // String domain = 'https://9c23-1-2-245-217.ap.ngrok.io';
+  String domain = 'http://www.leaflet.secret-serv.com';
   MyConstant();
 }
